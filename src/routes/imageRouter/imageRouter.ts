9423461928041c38cf7af5901191f01b932ea51e
Kyle Davis <kyledavisdev@gmail.com>
2022-04-28 12:09:48 -0500

    Added description above routes, add base route

--- src/routes/imageRouter/imageRouter.ts
+++ src/routes/imageRouter/imageRouter.ts
@@ -2,8 +2,19 @@ import express, { NextFunction, Request, Response } from "express";
 
 const router = express.Router();
 
-//** Reminder: All routes in here prefixed with /images **//
+/** @route GET /images
+ *  @description Grab metadata about all available images
+ *  @return JSON of image metadata
+ */
+router.get("/", async (req: Request, res: Response) => {
+  res.status(200).send("success!");
+});
 
+/** @route GET /images/:id
+ *  @description Grab metadata about a specific image
+ *  @param {String} req.body.id - unique id of the image
+ *  @return JSON of image metadata for the specified image
+ */
 router.get("/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
   res.send("good job" + id);
