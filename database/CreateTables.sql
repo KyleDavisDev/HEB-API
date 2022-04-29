871018cc4eedd1b3c0581e64bff88ff96d9614ca
Kyle Davis <kyledavisdev@gmail.com>
2022-04-28 23:00:08 -0500

    Update for mysql

--- database/CreateTables.sql
+++ database/CreateTables.sql
@@ -1,40 +1,40 @@
-CREATE TABLE Images (
-    Id int(11) NOT NULL AUTO_INCREMENT,
-    TypeId int(11) NOT NULL,
-    Label varchar(100) NULL,
-    Path varchar(150) NOT NULL,
-    CreateDate bigint(20) unsigned DEFAULT NOT NULL,
+CREATE TABLE ImageTypes (
+    Id INT NOT NULL AUTO_INCREMENT,
+    Value varchar(150) NOT NULL,
+    CreateDate bigint(20) NOT NULL,
     IsActive tinyint(1) DEFAULT '1',
     PRIMARY KEY (Id)
-    CONSTRAINT "Images_TypeId_ImageTypes_Id" FOREIGN KEY (TypeId) REFERENCES ImageTypes(Id)
 ) DEFAULT CHARSET=latin1;
 
-CREATE TABLE ImageTypes (
-    Id int(11) NOT NULL AUTO_INCREMENT,
-    Value varchar(150) NOT NULL,
-    CreateDate bigint(20) unsigned DEFAULT NOT NULL,
+CREATE TABLE Images (
+    Id INT NOT NULL AUTO_INCREMENT,
+    TypeId INT NOT NULL,
+    Label varchar(100) NULL,
+    Path varchar(150) NOT NULL,
+    CreateDate bigint(20) NOT NULL,
     IsActive tinyint(1) DEFAULT '1',
-    PRIMARY KEY (Id)
+    PRIMARY KEY (Id),
+    FOREIGN KEY (TypeId) REFERENCES ImageTypes(Id)
 ) DEFAULT CHARSET=latin1;
 
 CREATE TABLE ImageMetadata (
-    Id int(11) NOT NULL AUTO_INCREMENT,
-    ImageId int(11) NOT NULL,
+    Id INT NOT NULL AUTO_INCREMENT,
+    ImageId INT NOT NULL,
     Name varchar(100) NULL,
     Value varchar(150) NOT NULL,
-    CreateDate bigint(20) unsigned DEFAULT NOT NULL,
+    CreateDate bigint(20) NOT NULL,
     IsActive tinyint(1) DEFAULT '1',
-    PRIMARY KEY (Id)
-    CONSTRAINT "ImageMetadata_ImageId_Images_Id" FOREIGN KEY (ImageId) REFERENCES Images(Id)
+    PRIMARY KEY (Id),
+    FOREIGN KEY (ImageId) REFERENCES Images(Id)
 ) DEFAULT CHARSET=latin1;
 
 CREATE TABLE ImageObjects (
-    Id int(11) NOT NULL AUTO_INCREMENT,
-    ImageId int(11) NOT NULL,
+    Id INT NOT NULL AUTO_INCREMENT,
+    ImageId INT NOT NULL,
     Name varchar(100) NULL,
     Confidence varchar(150) NOT NULL,
-    CreateDate bigint(20) unsigned DEFAULT NOT NULL,
+    CreateDate bigint(20) NOT NULL,
     IsActive tinyint(1) DEFAULT '1',
-    PRIMARY KEY (Id)
-    CONSTRAINT "ImageObjects_ImageId_Images_Id" FOREIGN KEY (ImageId) REFERENCES Images(Id)
+    PRIMARY KEY (Id),
+    FOREIGN KEY (ImageId) REFERENCES Images(Id)
 ) DEFAULT CHARSET=latin1;
