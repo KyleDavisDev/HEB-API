d13e84a3b40d06ed861967060366a1bd8da681af
Kyle Davis <kyledavisdev@gmail.com>
2022-04-28 23:04:13 -0500

    init commit

---
+++ database/PopulateTables.sql
@@ -0,0 +1,6 @@
+INSERT INTO `ImageTypes`(`Value`, `CreateDate`, `IsActive`) VALUES ('png',UNIX_TIMESTAMP(NOW()),1);
+INSERT INTO `ImageTypes`(`Value`, `CreateDate`, `IsActive`) VALUES ('jpg',UNIX_TIMESTAMP(NOW()),1);
+INSERT INTO `ImageTypes`(`Value`, `CreateDate`, `IsActive`) VALUES ('jpeg',UNIX_TIMESTAMP(NOW()),1);
+INSERT INTO `ImageTypes`(`Value`, `CreateDate`, `IsActive`) VALUES ('gif',UNIX_TIMESTAMP(NOW()),1);
+INSERT INTO `ImageTypes`(`Value`, `CreateDate`, `IsActive`) VALUES ('tiff',UNIX_TIMESTAMP(NOW()),1);
+INSERT INTO `ImageTypes`(`Value`, `CreateDate`, `IsActive`) VALUES ('pdf',UNIX_TIMESTAMP(NOW()),1);
