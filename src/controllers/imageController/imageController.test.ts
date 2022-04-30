0432d0602ea9a1aad55968bb34e4698b3b7e84ef
Kyle Davis <kyledavisdev@gmail.com>
2022-04-30 14:15:27 -0500

    Check for multiple images to return

--- src/controllers/imageController/imageController.test.ts
+++ src/controllers/imageController/imageController.test.ts
@@ -1,12 +1,21 @@
 import { createMock } from "ts-auto-mock";
 import { imageRepository } from "./imageController";
 import { Context } from "../../Classes/Context/Context";
-import { Image } from "../../Models/Image";
 import { ImageTypes } from "../../Models/ImageTypes";
 import { ImageMetadata } from "../../Models/ImageMetadata";
 import { ImageObjects } from "../../Models/ImageObjects";
+import { ImageBuilder } from "../../Models/Builders/ImageBuilder";
+import { ImageTypeBuilder } from "../../Models/Builders/ImageTypeBuilder";
+import { Image } from "../../Models/Image";
+import { ImageMetadataBuilder } from "../../Models/Builders/ImageMetadataBuilder";
+import { ImageObjectBuilder } from "../../Models/Builders/ImageObjectBuilder";
 
 describe("ImageRepository", () => {
+  const _imageBuilder = new ImageBuilder();
+  const _typeBuilder = new ImageTypeBuilder();
+  const _metaDataBuilder = new ImageMetadataBuilder();
+  const _objectBuilder = new ImageObjectBuilder();
+
   describe("getByIdAsync", () => {
     it("should return null on invalid id", async () => {
       // Given
@@ -44,11 +53,9 @@ describe("ImageRepository", () => {
       //       how the data gets queried. I'd have to spend some more time thinking about this
       //       for larger implementations.
       const moqMetadata = createMock<ImageMetadata>();
-      const moqType = createMock<ImageTypes>();
+      const moqType = _typeBuilder.AnImageFromDB();
       const moqImageObjects = createMock<ImageObjects>();
-      const moqImage = createMock<Image>();
-      moqImage.Metadata = [moqMetadata];
-      moqImage.Objects = [moqImageObjects];
+      const moqImage = _imageBuilder.AnImageFromDB();
       const db: Context = createMock<Context>({
         queryAsync: () =>
           Promise.resolve([
@@ -64,7 +71,12 @@ describe("ImageRepository", () => {
       const result = await sut.getByIdAsync({ db, id });
 
       // Then
-      expect(result).toStrictEqual(moqImage);
+      // Would like to be able to match the 'shape' of Image but not seeing how to do it easily in jest
+      expect(result).not.toBeNull();
+      expect((result as Image).Id).not.toBeNull();
+      expect((result as Image).CreateDate).not.toBeNull();
+      expect((result as Image).IsActive).not.toBeNull();
+      expect((result as Image).Path).not.toBeNull();
     });
   });
 
@@ -75,10 +87,47 @@ describe("ImageRepository", () => {
       const sut = imageRepository;
 
       // When
-      const images = await imageRepository.getAllAsync({ db });
+      const images = await sut.getAllAsync({ db });
 
       // Then
       expect(images).toEqual([]);
+    });
+
+    it("should return set of available images", async () => {
+      // Given
+      const fakeImage = _imageBuilder.AnImageFromDB();
+      const fakeType = _typeBuilder.AnImageFromDB();
+      const fakeMetadata = [
+        _metaDataBuilder.Random().WithImageIdOf(fakeImage["Images.Id"]).Build(),
+        _metaDataBuilder.Random().WithImageIdOf(fakeImage["Images.Id"]).Build(),
+        _metaDataBuilder.Random().WithImageIdOf(fakeImage["Images.Id"]).Build(),
+      ];
+      const fakeObjects = [
+        _objectBuilder.Random().WithImageIdOf(fakeImage["Images.Id"]).Build(),
+        _objectBuilder.Random().WithImageIdOf(fakeImage["Images.Id"]).Build(),
+        _objectBuilder.Random().WithImageIdOf(fakeImage["Images.Id"]).Build(),
+      ];
+      const db: Context = createMock<Context>({
+        queryAsync: () =>
+          Promise.resolve([
+            [{ ...fakeImage, ...fakeType }],
+            [fakeMetadata],
+            [fakeObjects],
+          ]),
+      });
+      const sut = imageRepository;
+
+      // When
+      const images: Image[] = await sut.getAllAsync({ db });
+
+      // Then
+      // Would like to be able to match the 'shape' of Image but not seeing how to do it easily in jest
+      expect(images).not.toBeNull();
+      expect(images[0]).not.toBeNull();
+      expect(images[0].Id).not.toBeNull();
+      expect(images[0].CreateDate).not.toBeNull();
+      expect(images[0].IsActive).not.toBeNull();
+      expect(images[0].Path).not.toBeNull();
     });
   });
 });
