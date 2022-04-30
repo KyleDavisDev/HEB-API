0d9a6b51132d999f1465673ef329ca10585f9a41
Kyle Davis <kyledavisdev@gmail.com>
2022-04-30 14:48:47 -0500

    use jest to mock out imageRepository, general cleanup

--- src/routes/imageRouter/imageRouter.test.ts
+++ src/routes/imageRouter/imageRouter.test.ts
@@ -1,60 +1,75 @@
 import axios from "axios";
+
+import { imageRepository } from "../../repositories/imageRepository/imageRepository";
 require("dotenv").config({ path: "variables.env" });
 
+const nullJestFn = jest.fn(() => null);
+const getByIdAsyncFake = nullJestFn;
+const emptyJestFn = jest.fn(() => []);
+const getAllAsyncFake = emptyJestFn;
+jest.mock("../../repositories/imageRepository/imageRepository", () => {
+  return { getByIdAsync: getByIdAsyncFake, getAllAsync: getAllAsyncFake };
+});
+
 describe("/images", () => {
-  jest.setTimeout(10000);
+  jest.setTimeout(10000); // increase timeout
   const port = parseInt(<string>process.env.DEFAULT_PORT, 10) || 8080;
-  const basePath = `http://localhost:${port}/images`;
+  const basePath = `http://localhost:${port}`;
+  const route = "images";
 
-  it("should return 200 status", async () => {
-    //Given
-    const status = 200;
-
-    //When
-    const result = await axios.get(basePath);
-
-    //Then
-    expect(result.status).toEqual(200);
-  });
-
-  // it("/:id should accept id as a query parameter 200 status", async () => {
-  //   //Given
-  //   const id = 5;
-  //   const status = 200;
-  //
-  //   //When
-  //   const result = await axios.get(`${basePath}/${id}`);
-  //
-  //   //Then
-  //   expect(result.status).toEqual(status);
-  // });
-
-  it("/:id should return 301 if id is not a positive numeric value", async () => {
-    //Given
-    const invalidIds = [
-      "a",
-      "b",
-      "abc",
-      "test",
-      ["abc", "123"],
-      [1, 2, 3],
-      0,
-      -1,
-      3.1415,
-      Math.PI,
-    ];
-    const status = 301;
-
-    for (let i = 0; i < invalidIds.length; i++) {
-      const id = invalidIds[i];
+  describe("/", () => {
+    it("should return 200 status", async () => {
+      //Given
+      const status = 200;
 
       //When
-      const result = await axios
-        .get(`${basePath}/${id}`)
-        .catch((x) => x.response);
+      const result = await axios.get(basePath);
 
       //Then
       expect(result.status).toEqual(status);
-    }
+    });
+  });
+
+  describe("/:id", () => {
+    it("should accept id as a query parameter and return with 200 status", async () => {
+      //Given
+      const id = 5;
+      const status = 200;
+
+      //When
+      const result = await axios.get(`${basePath}/${route}/${id}`);
+
+      //Then
+      expect(result.status).toEqual(status);
+    });
+
+    it("should return 301 if id is not a positive numeric value", async () => {
+      //Given
+      const invalidIds = [
+        "a",
+        "b",
+        "abc",
+        "test",
+        ["abc", "123"],
+        [1, 2, 3],
+        0,
+        -1,
+        3.1415,
+        Math.PI,
+      ];
+      const status = 301;
+
+      for (let i = 0; i < invalidIds.length; i++) {
+        const id = invalidIds[i];
+
+        //When
+        const result = await axios
+          .get(`${basePath}/${route}/${id}`)
+          .catch((x) => x.response);
+
+        //Then
+        expect(result.status).toEqual(status);
+      }
+    });
   });
 });
