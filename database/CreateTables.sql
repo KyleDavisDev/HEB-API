fe7f42c22eccecd2bc7e42b84d8fb206a6e036a7
Kyle Davis <kyledavisdev@gmail.com>
2022-04-30 12:49:58 -0500

    Update field to decimal

--- database/CreateTables.sql
+++ database/CreateTables.sql
@@ -32,7 +32,7 @@ CREATE TABLE ImageObjects (
     Id INT NOT NULL AUTO_INCREMENT,
     ImageId INT NOT NULL,
     Name varchar(100) NULL,
-    Confidence varchar(150) NOT NULL,
+    Confidence decimal(10,0) NOT NULL,
     CreateDate bigint(20) NOT NULL,
     IsActive tinyint(1) DEFAULT '1',
     PRIMARY KEY (Id),
