6fffb807921f7c363de27e59719a7e85b599bd14
Kyle Davis <kyledavisdev@gmail.com>
2022-04-30 23:52:30 -0500

    start to piece tother strategy for saving the image via cloudinary

--- src/Classes/Context/FileContext.ts
+++ src/Classes/Context/FileContext.ts
@@ -1,7 +1,13 @@
+require("dotenv").config({ path: "variables.env" });
 import { Context } from "./Context";
+import { v2 as cloudinary } from "cloudinary"; // image hosting
 
-// Intentionally left bare.
-// Added to demonstrate the interfaces and singleton pattern
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+
 class FileContext implements Context {
   private static _instance: FileContext;
 
@@ -19,6 +25,22 @@ class FileContext implements Context {
   queryAsync(sql: string, args: string): Promise<any> {
     return Promise.resolve(undefined);
   }
+
+  saveImageAsync = async (image: string): Promise<string | null> => {
+    console.log("I MADE IT TO THE REPO");
+
+    // Upload image
+    const img = await cloudinary.uploader.upload(image, {
+      folder: process.env.CLOUDINARY_FOLDER,
+      quality: "auto",
+      fetch_format: "auto",
+    });
+
+    console.log(img);
+
+    return Promise.resolve(null);
+  };
 }
 
-module.exports = FileContext.getInstance();
+const temp = FileContext.getInstance();
+export { temp as FileContext };
