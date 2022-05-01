4abc8711072a619a0245d76dc388967606469a94
Kyle Davis <kyledavisdev@gmail.com>
2022-04-30 19:01:54 -0500

    Init starter file

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { imageRepositoryImpl } from "./repositories/imageRepository/imageRepository";
+
+require("dotenv").config({ path: "variables.env" });
+import { serverSetup } from "./serverSetup/serverSetup";
+
+// grab port from .env file or assign default
+const port: number = parseInt(<string>process.env.DEFAULT_PORT, 10) || 8080;
+const app = serverSetup({ imageRepository: imageRepositoryImpl });
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
