c3052f9b84e4849e9a77cab6b63804a0f4dfaa4c
Kyle Davis <kyledavisdev@gmail.com>
2022-05-02 15:10:49 -0500

    Ignore Context files since those don't really need to be tested

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  collectCoverageFrom: ["src/**/*.ts", "src/*.ts", "!src/**/*Context.ts"],
   globals: {
     "ts-jest": {
       compiler: "ttypescript",
